5a0fa78e1d06cf1db960a91268a937b88f99b21c
Thonedra <thonedra.dev@gmail.com>
2026-09-23 04:15:22 +0800

    New Page is connected with navigation

--- src/Components/NavControls.jsx
+++ src/Components/NavControls.jsx
@@ -91,6 +91,7 @@ export default function NavControls({
         <button
           type="button"
           className="nav-icon-btn nav-controls-tab"
+          onClick={() => navigate('/calendar')}
           title="Calendar"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
